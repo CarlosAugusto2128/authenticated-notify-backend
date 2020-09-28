16c9d2953d2feba3d8a3ba6c24c2329077e8d100
Carlos Augusto <carlosass04.5@gmail.com>
2020-09-28 15:36:29 -0300

    Update: Creating Controller and Route  for creating user and Add hook of beforeSave on User Model for password cryptography using bcrypt lib.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,7 @@
 import Sequelize, { Model } from 'sequelize';
 
+import bcrypt from 'bcryptjs';
+
 class User extends Model {
   static init(sequelize) {
     super.init(
@@ -7,10 +9,17 @@ class User extends Model {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         first_auth: Sequelize.DATE,
       },
       { sequelize }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
 
     return this;
   }
