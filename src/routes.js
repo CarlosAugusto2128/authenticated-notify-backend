92a1f2e4ec24d13819ab010d05922e7b10f90e37
Carlos Augusto <carlosaugustoss2128@gmail.com>
2020-09-30 12:50:10 -0300

    Update: Have been added the file of NotificationController and your Routes for Update and List. Have been added too the middleware of Authentication.

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,18 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
+import NotificationController from './app/controllers/NotificationController';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
